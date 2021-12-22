0161fa2c77733a222b0b7f33da46cc1cd84c4a02
Sri lakshmi kanthan P <srilakshmikanthanp@gmail.com>
2021-12-22 12:36:43 +0530

    Add BasicView

--- src/main/java/com/github/srilakshmikanthanp/facsimile/datum/Mapping.java
+++ src/main/java/com/github/srilakshmikanthanp/facsimile/datum/Mapping.java
@@ -5,7 +5,145 @@
 
 package com.github.srilakshmikanthanp.facsimile.datum;
 
-public class Mapping 
-{
+import javax.crypto.*;
+import java.nio.file.*;
+import java.util.HashMap;
 
+
+/**
+ * Class That handles the encrypion and decryption of the data.
+ */
+class Crypto
+{
+    // base dir for data
+    private Path baseDir;
+    
+    // key for encryption
+    private SecretKey key;
+
+    /**
+     * Constructor for the Crypto class.
+     * 
+     * @param baseDir The base directory for the data.
+     */
+    public Crypto(Path baseDir)
+    {
+        this.baseDir = baseDir;
+    }
+
+    /**
+     * Authorize the user.
+     * 
+     * @param password the password.
+     * @throws IlligalArgumentException if password was incorrect.
+     */
+    public void auth(String password) throws IllegalArgumentException
+    {
+
+    }
+
+    /**
+     * Encrypt the data.
+     * 
+     * @param data the data to be encrypted.
+     */
+    public String encrypt(String text)
+    {
+        return "";
+    }
+
+    /**
+     * Decrypt the data.
+     * 
+     * @param data the data to be decrypted.
+     */
+    public String decrypt(String text)
+    {
+        return "";
+    }
+}
+
+/**
+ * Mapping of the datum key to the value objects.
+ */
+public class Mapping extends HashMap<String, String>
+{
+    // base dir for data
+    private Path baseDir;
+
+    // Crypto object
+    private Crypto crypto;
+
+    // is authorized
+    private boolean authorized;
+
+    /**
+     * Saves the data to presistant storage.
+     */
+    private void save()
+    {
+
+    }
+
+    /**
+     * Loads the data from presistant storage.
+     */
+    private void load()
+    {
+
+    }
+
+    /**
+     * Constructor of Mapping.
+     * 
+     * @param baseDir the base directory of the datum. 
+     */
+    public Mapping(Path baseDir)
+    {
+        this.baseDir = baseDir;
+    }
+
+    /**
+     * Authorize the datum.
+     * 
+     * @param password the password.
+     * @throws IlligalArgumentException if password was incorrect.
+     */
+    public void auth(String password) throws IllegalArgumentException
+    {
+
+    }
+
+    /**
+     * Is the mapping authorized.
+     */
+    public boolean isAuthorized()
+    {
+        return authorized;
+    }
+
+    /**
+     * Get the value of the key.
+     * 
+     * @param key the key.
+     * @return the value of the key.
+     */
+    @Override
+    public String get(Object key)
+    {
+        return "";
+    }
+
+    /**
+     * Set the value of the key.
+     * 
+     * @param key the key.
+     * @param value the value of the key.
+     * @return the value of the key.
+     */
+    @Override
+    public String put(String key, String value)
+    {
+        return "";
+    }
 }
