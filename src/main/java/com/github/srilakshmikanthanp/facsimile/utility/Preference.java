006eb1ff707f5e78452eb276b24bb8a3b70e0c18
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2021-12-25 15:58:12 +0530

    Add mask one

--- src/main/java/com/github/srilakshmikanthanp/facsimile/utility/Preference.java
+++ src/main/java/com/github/srilakshmikanthanp/facsimile/utility/Preference.java
@@ -6,7 +6,40 @@
 package com.github.srilakshmikanthanp.facsimile.utility;
 
 
+import java.util.prefs.*;
+
+
+/**
+ * This class is used to store and retrieve the preferences the application.
+ */
 public class Preference 
 {
-        
+    /**
+     * PReference node for the application.
+     */
+    public static Preferences prefs = Preferences.userNodeForPackage(Preference.class);
+
+    // Preference Mask one
+    public static String MASK_ONE_KEY = "MASK_ONE";
+    public static String MASK_ONE_DEFAULT = "CTRL";
+
+    /**
+     * Get the mask one.
+     * 
+     * @return the mask one.
+     */
+    public static String getMaskOne()
+    {
+        return prefs.get(MASK_ONE_KEY, MASK_ONE_DEFAULT);
+    }
+
+    /**
+     * Set the mask one.
+     * 
+     * @param maskOne the mask one.
+     */
+    public static void setMaskOne(String maskOne)
+    {
+        prefs.put(MASK_ONE_KEY, maskOne);
+    }
 }
