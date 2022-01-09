80c42fab22d6e0ad2649ce9b095f65bf5799bda4
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2022-01-09 19:10:43 +0530

    Complete the dialogs

--- src/main/java/com/github/srilakshmikanthanp/facsimile/dialog/InputShortCut.java
+++ src/main/java/com/github/srilakshmikanthanp/facsimile/dialog/InputShortCut.java
@@ -1,18 +1,87 @@
 package com.github.srilakshmikanthanp.facsimile.dialog;
 
-import javafx.scene.Node;
-import javafx.stage.Window;
+import java.util.Arrays;
+
+import javafx.geometry.*;
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.scene.image.*;
+import javafx.scene.input.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+
 
 /**
  * PAssword Input Dialog.
  */
 public class InputShortCut extends AbstractDialog {
+    // Mask one of short cut
+    private ChoiceBox<String> maskOne = new ChoiceBox<>();
+
+    // Mask two of short cut
+    private ChoiceBox<String> maskTwo = new ChoiceBox<>();
+
+    // Key code of short cut
+    private ChoiceBox<String> keycode = new ChoiceBox<>();
+
+    // label for theShort
+    private Label label = new Label("Shortcut");
+
+    // status of the dialog
+    private boolean isOkay = false;
+
     /**
      * Get the Content pane.
      */
     @Override
     protected Node getContent() {
-        return null;
+        // init the label
+        var image = new ImageView(
+            getClass().getResource("/images/shortcut.png").toString()
+        );
+        image.setFitHeight(100);
+        image.setFitWidth(100);
+        label.setGraphic(image);
+        label.setContentDisplay(ContentDisplay.TOP);
+
+        // init the mask one
+        maskOne.getItems().addAll(
+            // TODO: add the mask one
+        );
+
+        // init the mask two
+        maskTwo.getItems().addAll(
+            // TODO: add the mask two
+        );
+
+        // init the keycode
+        var list = Arrays.asList(
+            KeyCode.values()
+        ).stream().map(
+            (key) -> key.getName().toUpperCase()
+        ).collect(
+            java.util.stream.Collectors.toList()
+        );
+
+        // Sort the list
+        list.sort((a, b) -> a.compareTo(b));
+        
+        // TODO: remove the keycode
+
+        // add the keycode
+        keycode.getItems().addAll(
+            list
+        );
+
+        // define the vbox
+        var vbox = new VBox(label, maskOne, maskTwo, keycode);
+
+        // init the vbox
+        vbox.setSpacing(10);
+        vbox.setAlignment(Pos.CENTER);
+
+        // done
+        return vbox;
     }
 
     /**
@@ -20,7 +89,8 @@ public class InputShortCut extends AbstractDialog {
      */
     @Override
     protected void okayPressed() {
-        
+        this.isOkay = true;
+        this.hide();
     }
 
     /**
@@ -28,7 +98,8 @@ public class InputShortCut extends AbstractDialog {
      */
     @Override
     protected void cutPressed() {
-
+        this.isOkay = false;
+        this.hide();
     }
 
     /**
@@ -38,5 +109,26 @@ public class InputShortCut extends AbstractDialog {
      */
     public InputShortCut(Window owner) {
         super(owner);
-    }    
+    }
+
+    /**
+     * Get the status of the dialog.
+     */
+    public boolean isOkay() {
+        return this.isOkay;
+    }
+
+    /**
+     * Get the mask one.
+     */
+    public String getMaskOne() {
+        return this.maskOne.getValue();
+    }
+
+    /**
+     * Get the mask two.
+     */
+    public String getMaskTwo() {
+        return this.maskTwo.getValue();
+    }
 }
